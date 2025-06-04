712a7cfcdcff2f3491457d61fce57f403c14a025
Ryehean <ryehean@gmail.com>
2025-06-04 11:25:52 -0400

    Added login, register and user menu components and functionality

--- API/Data/StoreContext.cs
+++ API/Data/StoreContext.cs
@@ -1,10 +1,12 @@
 using System;
 using API.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Data;
 
-public class StoreContext(DbContextOptions options) : DbContext(options)
+public class StoreContext(DbContextOptions options) : IdentityDbContext<User>(options)
 {
     public required DbSet<Product> Products { get; set; }
 
@@ -13,5 +15,16 @@ public class StoreContext(DbContextOptions options) : DbContext(options)
 
 
     public required DbSet<Cart> Carts { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        builder.Entity<IdentityRole>()
+        .HasData(
+            new IdentityRole { Id = "40473259-2369-4f02-9731-158578f4f445", Name = "Member", NormalizedName = "MEMBER" },
+            new IdentityRole { Id = "40866ae7-dfde-4c22-b872-e0ea532beed5", Name = "Admin", NormalizedName = "ADMIN" }
+        );
+    }
 
 }
--- reactClient/src/app/layout/NavBar.tsx
+++ reactClient/src/app/layout/NavBar.tsx
@@ -4,20 +4,22 @@ import { Link, NavLink } from "react-router";
 import { useAppDispatch, useAppSelector } from "../store/store";
 import { setDarkMode } from "./uiSlice";
 import { useFetchCartQuery } from "../../features/cart/cartApi";
+import UserMenu from "./UserMenu";
+import { useUserInfoQuery } from "../../features/account/accountApi";
 
 const midLinks = [
-  {title: 'catalog', path: '/catalog'},
-  {title: 'about', path: '/about'},
-  {title: 'contact', path: '/contact'}
+  { title: 'catalog', path: '/catalog' },
+  { title: 'about', path: '/about' },
+  { title: 'contact', path: '/contact' }
 ]
 
 const rightLinks = [
-  {title: 'login', path: '/login'},
-  {title: 'register', path: '/register'}
+  { title: 'login', path: '/login' },
+  { title: 'register', path: '/register' }
 ]
 
 const navStyles = {
-  color: 'inherit', 
+  color: 'inherit',
   typography: 'h6',
   textDecoration: 'none',
   '&:hover': {
@@ -29,44 +31,48 @@ const navStyles = {
 }
 
 export default function NavBar() {
-  const {isLoading, darkMode} = useAppSelector(state => state.ui);
+  const { data: user } = useUserInfoQuery();
+  const { isLoading, darkMode } = useAppSelector(state => state.ui);
   const dispatch = useAppDispatch();
-  const {data: cart} = useFetchCartQuery();
+  const { data: basket } = useFetchCartQuery();
 
-  const itemCount = cart?.items.reduce((sum, item)=> sum + item.quantity, 0) || 0;
+  const itemCount = basket?.items.reduce((sum, item) => sum + item.quantity, 0) || 0;
 
   return (
-    <AppBar position='fixed'>
-        <Toolbar sx={{display: 'flex', justifyContent: 'space-between', alignItems: 'center'}}>
-          <Box display='flex' alignItems='center'>
-            <Typography component={NavLink} sx={navStyles} to='/' variant='h6'>React Store</Typography>
-              <IconButton onClick={()=> dispatch(setDarkMode())}>
-                  {darkMode ? <DarkMode /> : <LightMode sx={{color: 'yellow'}}/>}
-              </IconButton>
-          </Box>
-            
-          <List sx={{display: 'flex'}}>
-            {midLinks.map(({title, path}) => (
-              <ListItem
-                component={NavLink}
-                to={path}
-                key={path}
-                sx={navStyles}
-              >
-                {title.toUpperCase()}
-              </ListItem>
-            ))}
-          </List>
+    <AppBar position="fixed">
+      <Toolbar sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+        <Box display='flex' alignItems='center'>
+          <Typography component={NavLink} sx={navStyles} to='/' variant="h6">RE-STORE</Typography>
+          <IconButton onClick={() => dispatch(setDarkMode())}>
+            {darkMode ? <DarkMode /> : <LightMode sx={{ color: 'yellow' }} />}
+          </IconButton>
+        </Box>
 
-          <Box display='flex' alignItems='center'>
-            <IconButton component={Link} to='/cart' size='large'>
-              <Badge badgeContent={itemCount} color="secondary">
-                <ShoppingCart />
-              </Badge>
-            </IconButton>  
+        <List sx={{ display: 'flex' }}>
+          {midLinks.map(({ title, path }) => (
+            <ListItem
+              component={NavLink}
+              to={path}
+              key={path}
+              sx={navStyles}
+            >
+              {title.toUpperCase()}
+            </ListItem>
+          ))}
+        </List>
 
-            <List sx={{display: 'flex'}}>
-              {rightLinks.map(({title, path}) => (
+        <Box display='flex' alignItems='center'>
+          <IconButton component={Link} to='/basket' size="large" sx={{ color: 'inherit' }}>
+            <Badge badgeContent={itemCount} color="secondary">
+              <ShoppingCart />
+            </Badge>
+          </IconButton>
+
+          {user ? (
+            <UserMenu user={user} />
+          ) : (
+            <List sx={{ display: 'flex' }}>
+              {rightLinks.map(({ title, path }) => (
                 <ListItem
                   component={NavLink}
                   to={path}
@@ -77,14 +83,17 @@ export default function NavBar() {
                 </ListItem>
               ))}
             </List>
-          </Box>            
-            
-        </Toolbar>
-        {isLoading && (
-          <Box sx={{width: '100%'}}>
-            <LinearProgress color="secondary" />
-          </Box>
-        )}
+          )}
+
+
+        </Box>
+
+      </Toolbar>
+      {isLoading && (
+        <Box sx={{ width: '100%' }}>
+          <LinearProgress color="secondary" />
+        </Box>
+      )}
     </AppBar>
   )
 }
